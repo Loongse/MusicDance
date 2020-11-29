7aba723ac97dbbde17b97f19837308ed34c89973
loongse <18707950895@163.com>
2020-11-30 00:37:25 +0800

    viewPager以及指示器初始化

---
+++ md_voice/src/main/java/com/md/md_voice/view/discory/DiscoryFragment.java
@@ -0,0 +1,50 @@
+package com.md.md_voice.view.discory;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.md.md_voice.R;
+import com.md.md_voice.view.mine.MineFragment;
+
+public class DiscoryFragment extends Fragment {
+    private Context mContext;
+    private TextView mTextView;
+
+    public DiscoryFragment() {
+        // Required empty public constructor
+    }
+
+    // TODO: Rename and change types and number of parameters
+    public static MineFragment newInstance() {
+        MineFragment fragment = new MineFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mContext = getActivity();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_mine, null);
+        mTextView = rootView.findViewById(R.id.textView);
+        mTextView.setText("发现");
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //发请求更新UI
+    }
+}
--- md_voice/src/main/java/com/md/md_voice/view/home/HomeActivity.java
+++ md_voice/src/main/java/com/md/md_voice/view/home/HomeActivity.java
@@ -1,15 +1,29 @@
 package com.md.md_voice.view.home;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.view.View;
 
+import com.md.lib_common_ui.pager_indictor.ScaleTransitionPagerTitleView;
 import com.md.md_voice.R;
+import com.md.md_voice.view.home.adapter.HomePagerAdapter;
 import com.md.md_voice.view.home.model.CHANNEL;
 
+import net.lucode.hackware.magicindicator.MagicIndicator;
+import net.lucode.hackware.magicindicator.ViewPagerHelper;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.CommonNavigator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.CommonNavigatorAdapter;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerIndicator;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.abs.IPagerTitleView;
+import net.lucode.hackware.magicindicator.buildins.commonnavigator.titles.SimplePagerTitleView;
+
 public class HomeActivity extends FragmentActivity implements View.OnClickListener {
+    //指示首页需要出现的卡片
     private static final CHANNEL[] CHANNELS =
             new CHANNEL[]{
                     CHANNEL.MY,
@@ -25,6 +39,7 @@ public class HomeActivity extends FragmentActivity implements View.OnClickListen
     private View mToggleView;
     private View mSearchView;
     private ViewPager mViewPager;
+    private HomePagerAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +58,53 @@ public class HomeActivity extends FragmentActivity implements View.OnClickListen
         mToggleView = findViewById(R.id.toggle_view);
         mToggleView.setOnClickListener(this);
         mSearchView = findViewById(R.id.search_view);
+
+        mViewPager = findViewById(R.id.view_pager);
+        mAdapter = new HomePagerAdapter(getSupportFragmentManager(), CHANNELS);
+        mViewPager.setAdapter(mAdapter);
+        initMagicIndicator();
+    }
+
+    // 初始化指示器
+    private void initMagicIndicator() {
+        MagicIndicator magicIndicator = findViewById(R.id.magic_indicator);
+        magicIndicator.setBackgroundColor(Color.WHITE);
+        CommonNavigator commonNavigator = new CommonNavigator(this);
+        commonNavigator.setAdapter(new CommonNavigatorAdapter() {
+            @Override
+            public int getCount() {
+                return CHANNELS == null ? 0 : CHANNELS.length;
+            }
+
+            @Override
+            public IPagerTitleView getTitleView(Context context, int index) {
+                SimplePagerTitleView simplePagerTitleView = new ScaleTransitionPagerTitleView(context);
+                simplePagerTitleView.setText(CHANNELS[index].getValue());
+                simplePagerTitleView.setTextSize(19);
+                simplePagerTitleView.setTypeface(Typeface.defaultFromStyle(Typeface.BOLD));
+                simplePagerTitleView.setNormalColor(Color.parseColor("#999999"));
+                simplePagerTitleView.setSelectedColor(Color.parseColor("#333333"));
+                simplePagerTitleView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        mViewPager.setCurrentItem(index);
+                    }
+                });
+                return simplePagerTitleView;
+            }
+
+            @Override
+            public IPagerIndicator getIndicator(Context context) {
+                return null;
+            }
+
+            @Override
+            public float getTitleWeight(Context context, int index) {
+                return 1.0f;
+            }
+        });
+        magicIndicator.setNavigator(commonNavigator);
+        ViewPagerHelper.bind(magicIndicator, mViewPager);
     }
 
     @Override
